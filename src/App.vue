8771ff4f37aacaee6355772050c8c40c19cc0870
bk-mint <bearkim@dkant.net>
2021-05-19 02:47:55 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <button @click="getUserData">Get User data</button>
+    <table>
+      <tr v-for="(row, idx) in data" :key="idx">
+        <td>{{row.NAME}}</td>
+        <td>{{row.PHONE}}</td>
+        <td>{{row.EMAIL}}</td>
+        <td>{{row.BIRTHDAY}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: [{
+        NAME:"dummy", PHONE:"none", EMAIL:"no", BIRTHDAY:1111
+      }]
+    };
+  },
+  methods: {
+    getUserData() {
+      this.data = window.ipcRenderer.sendSync("get-user-list", null);
+    }
+  }
+};
+</script>
---
+++ vue.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+  runtimeCompiler: true,
+  productionSourceMap: false,
+  pluginOptions: {
+    electronBuilder: {
+      preload: "./src/preload.js",
+      externals: ["knex", "sqlite3"],
+      productName: "vue electron sqlite3",
+      builderOptions: {
+        asar: false,
+        extraResources: [
+          {
+            from: "./extraResources/",
+            to: "extraResources",
+            filter: ["**/*"]
+          }
+        ]
+      }
+    }
+  }
+};
